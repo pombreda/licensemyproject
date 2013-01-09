457d77f44b7298d5f208cda3587904f573ed16a8
Chris Missal <chris.missal@gmail.com>
2013-01-08 22:51:00 -0600

    turned application into a non-DB app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,11 +23,15 @@ Site::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Note: not running a Database at this point, no need for this now. CM 1/8/13
+  #config.active_record.mass_assignment_sanitizer = :strict
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Note: not running a Database at this point, no need for this now. CM 1/8/13
+  #config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
